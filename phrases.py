fd1c2da8406740a66d4e7cab31f54549b5a79997
Jeff An <uwjeffan@outlook.com>
2018-07-14 13:42:26 -0400

    add end of phrase words and basic implementation of phraseFor

--- phrases.py
+++ phrases.py
@@ -1,2 +1,64 @@
-def phrase_for(*, num_syllables, is_first_syllable_stressed):
-    assert False
+import random
+
+# Phrases that end the line
+end_phrases = {
+    1: [
+        "made",
+        "found",
+        "caught",
+        "seen",
+        "torn",
+        "proved"
+        "loved",
+        "pure",
+        "white",
+    ],
+    2: [
+        "golden",
+        "still dark",
+        "bloodstained",
+        "sun soaked",
+        "afoot",
+        "of gods",
+        "primrose",
+        "bedazzled",
+    ],
+    3: [
+        "sick at heart",
+        "seemeth pure",
+        "rose distill'd",
+        "heart assured",
+        "noble Feridun"
+        "beauteous geese",
+    ],
+    4: [
+        "surmounted high",
+        "noble and valiant",
+        ", a sorry sight",
+        "of naked truth",
+        "in one fell swoop",
+        "from jaws of death",
+        "beauty untrimm'd",
+    ],
+    5: [
+        "with surcease success",
+        "love as swift as thought",
+        "o're the castle flew",
+    ],
+    6: [
+        "as boundless as the sea",
+        "thy eternal summer",
+        "the darling buds of May",
+    ],
+    7: [
+        "nor reason can my passion hide",
+    ]
+}
+
+def pick_random(array):
+    return array[random.randint(0, len(array) - 1)]
+
+def phrase_for(*, num_syllables, is_first_syllable_stressed, pos_in_line):
+    return pick_random(end_phrases[num_syllables])
+    
+
